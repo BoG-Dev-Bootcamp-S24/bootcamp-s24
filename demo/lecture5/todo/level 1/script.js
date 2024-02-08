4c77f278ce62f36cb76905953f799ba72138cdac
arnav chintawar <arnavch04@gmail.com>
2024-02-08 18:45:49 -0500

    added demo 5

--- demo/lecture5/todo/level 1/script.js
+++ demo/lecture5/todo/level 1/script.js
@@ -1,16 +1,2 @@
 //solution
 
-document.getElementById('add-button').addEventListener('click', function() {
-    var todoList = document.getElementById('todo-list');
-    var todoInput = document.getElementById('todo-input');
-    var newTodo = todoInput.value;
-    
-    if (newTodo.trim() !== '') {
-        var listItem = document.createElement('li');
-        listItem.textContent = newTodo;
-        todoList.appendChild(listItem);
-        todoInput.value = ''; // Clear the input field
-    } else {
-        alert('Please enter a task.');
-    }
-});
